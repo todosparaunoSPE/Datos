fd77efa5c75d1ce5d7859dc90da12dcb79bec605
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-12-20 12:59:11 -0600

    Update app9.py

--- app9.py
+++ app9.py
@@ -14,6 +14,23 @@ import plotly.express as px
 import plotly.graph_objects as go
 from scipy.stats import norm
 
+
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 # Cargar automáticamente el archivo ponderado.xlsx
 archivo = 'ponderado.xlsx'
 
